fbca5910a4a1f1d67b988b35b49e2bc25340adf6
Michael Bel <michael-bel@outlook.com>
2016-04-08 15:50:19 +0300

    * edit layout background

--- app/src/main/java/org/app/application/SwitchFragment.java
+++ app/src/main/java/org/app/application/SwitchFragment.java
@@ -9,8 +9,6 @@ import android.widget.FrameLayout;
 
 public class SwitchFragment extends Fragment {
 
-    public SwitchFragment() {}
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,7 +17,10 @@ public class SwitchFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         FrameLayout layout = new FrameLayout(getActivity());
-        layout.setBackgroundColor(0xffffffff);
+
+        //Switch mSwitch = new Switch(getActivity());
+        //layout.addView(mSwitch, LayoutHelper.makeFrame(LayoutHelper.WRAP_CONTENT, LayoutHelper.WRAP_CONTENT, 20, 100, 0, 0));
+
         return layout;
     }
 }
