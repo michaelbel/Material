f1a46ce092b74de263bfe946acb2aad6b849456f
Michael Bel <michael-bel@outlook.com>
2016-04-10 02:22:16 +0300

    * add cardfragment to viewpager

--- app/src/main/java/org/app/application/MainActivity.java
+++ app/src/main/java/org/app/application/MainActivity.java
@@ -11,7 +11,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import org.app.application.Fragments.ListViewFragment;
+import org.app.material.AndroidUtilities;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,9 +20,6 @@ public class MainActivity extends AppCompatActivity {
 
     private static final int dotsMenu = 1;
     private static final int github = 2;
-
-    //private boolean menuOpen = false;
-    //private PopupMenu menu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,76 +34,35 @@ public class MainActivity extends AppCompatActivity {
         ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
         adapter.addFragment(new DialogFragment(), getResources().getString(R.string.Dialogs));
         adapter.addFragment(new ListViewFragment(), getResources().getString(R.string.ListView));
+        adapter.addFragment(new CardFragment(), getResources().getString(R.string.CardView));
         adapter.addFragment(new RecyclerFragment(), getResources().getString(R.string.RecyclerView));
+
         if (viewPager != null) {
             viewPager.setAdapter(adapter);
         }
 
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+
         if (tabLayout != null) {
             tabLayout.setupWithViewPager(viewPager);
         }
-
-        //menu = new PopupMenu(this);
-        //menu.setElevation(10);
-        //menu.setVisibility(View.INVISIBLE);
-        //menu.addItem(0, "Item 1");
-        //menu.addItem(1, "Item 2");
-        //menu.addItem(2, "Item 3");
-
-        //CoordinatorLayout layout = (CoordinatorLayout) findViewById(R.id.coordinator);
-        //if (layout != null) {
-        //    layout.addView(menu, LayoutHelper.makeCoordinator(180, LayoutHelper.WRAP_CONTENT, Gravity.TOP | Gravity.END, 0, 6, 6, 0));
-        //}
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        menu.add(0, github, Menu.NONE, "Open Github.com").setIcon(R.drawable.github_circle).setShowAsAction(1);
-        menu.add(0, dotsMenu, Menu.NONE, "Popup Menu").setIcon(R.drawable.dots_vertical).setShowAsAction(1);
+        menu.add(0, github, Menu.NONE, getResources().getString(R.string.OpenGithub)).setIcon(AndroidUtilities.getIcon(R.drawable.github_circle, 0xFFFFFFFF)).setShowAsAction(1);
+        menu.add(0, dotsMenu, Menu.NONE, getResources().getString(R.string.PopupMenu)).setIcon(R.drawable.dots_vertical).setShowAsAction(1);
         return super.onCreateOptionsMenu(menu);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == github) {
+            AndroidUtilities.openUrl(this, getResources().getString(R.string.GithubURL), 0xFF4285f4, R.drawable.abc_ic_menu_share_mtrl_alpha);
+        }
+
         return super.onOptionsItemSelected(item);
     }
-
-    //public void revealMenu() {
-    //    menuOpen = true;
-    //    int cx = menu.getRight();
-    //    int cy = menu.getTop();
-    //    int finalRadius = Math.max(menu.getWidth(), menu.getHeight());
-    //    Animator anim = ViewAnimationUtils.createCircularReveal(menu, cx, cy, 0, finalRadius);
-    //    anim.setDuration(500);
-    //    menu.setVisibility(View.VISIBLE);
-    //    anim.start();
-    //}
-
-    //public void hideMenu() {
-    //    menuOpen = false;
-    //    int cx = menu.getRight();
-    //    int cy = menu.getTop();
-    //    int initialRadius = menu.getWidth();
-    //    Animator anim = ViewAnimationUtils.createCircularReveal(menu, cx, cy, initialRadius, 0);
-    //    anim.addListener(new AnimatorListenerAdapter() {
-    //        @Override
-    //        public void onAnimationEnd(Animator animation) {
-    //            super.onAnimationEnd(animation);
-    //            menu.setVisibility(View.INVISIBLE);
-    //        }
-    //    });
-    //    anim.start();
-    //}
-
-    //@Override
-    //public void onBackPressed() {
-    //    if (menuOpen) {
-    //        hideMenu();
-    //    } else {
-    //        super.onBackPressed();
-    //    }
-    //}
 
     public class ViewPagerAdapter extends FragmentPagerAdapter {
 
