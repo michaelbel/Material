651e3af94033c79d9e13e1b28fe3cc545980af40
Michael Bel <michael-bel@outlook.com>
2016-04-10 02:20:31 +0300

    * new fragment for cardview

---
+++ app/src/main/java/org/app/application/CardFragment.java
@@ -0,0 +1,124 @@
+package org.app.application;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+
+import org.app.material.LayoutHelper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CardFragment extends Fragment {
+
+    public class ListItem {
+
+        public int icon;
+        public int dotsMenu;
+        public String title;
+        public String value;
+
+        public ListItem(int icon, String title, String value, int dotsMenu) {
+            this.icon = icon;
+            this.dotsMenu = dotsMenu;
+            this.title = title;
+            this.value = value;
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        FrameLayout layout = new FrameLayout(getActivity());
+        layout.setBackgroundColor(0xFFECEFF1);
+
+        List<ListItem> items = new ArrayList<>();
+        items.add(new ListItem(R.mipmap.ic_launcher, "Recycler Item 1", "Recycler Value 1", R.drawable.dots_vertical));
+        items.add(new ListItem(R.mipmap.ic_launcher, "Recycler Item 2", "Recycler Value 2", R.drawable.dots_vertical));
+        items.add(new ListItem(R.mipmap.ic_launcher, "Recycler Item 3", "Recycler Value 3", R.drawable.dots_vertical));
+        items.add(new ListItem(R.mipmap.ic_launcher, "Recycler Item 4", "Recycler Value 4", R.drawable.dots_vertical));
+        items.add(new ListItem(R.mipmap.ic_launcher, "Recycler Item 5", "Recycler Value 5", R.drawable.dots_vertical));
+
+        RecyclerViewAdapter adapter = new RecyclerViewAdapter(items, getActivity());
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+
+        RecyclerView recyclerView = new RecyclerView(getActivity());
+        recyclerView.setVerticalScrollBarEnabled(true);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(adapter);
+
+        layout.addView(recyclerView, LayoutHelper.makeFrame(LayoutHelper.MATCH_PARENT, LayoutHelper.MATCH_PARENT));
+        return layout;
+    }
+
+    public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.Holder> {
+
+        private Context mContext;
+        private List<ListItem> items;
+
+        private RecyclerViewAdapter(List<ListItem> items, Context context){
+            this.items = items;
+            this.mContext = context;
+        }
+
+        public class Holder extends RecyclerView.ViewHolder {
+
+            private Holder(View itemView) {
+                super(itemView);
+            }
+        }
+
+        @Override
+        public int getItemCount() {
+            return items.size();
+        }
+
+        @Override
+        public Holder onCreateViewHolder(ViewGroup viewGroup, int i) {
+            View view = new CardCell(mContext);
+            view.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    //int itemPosition = recyclerView.getChildPosition(v);
+                }
+            });
+            ((CardCell) view).setOnOptionsClick(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                }
+            });
+
+            Holder holder = new Holder(view);
+            return holder;
+        }
+
+        @Override
+        public void onBindViewHolder(Holder holder, int i) {
+            ListItem item = items.get(i);
+            ((CardCell) holder.itemView).addTitle(item.title).addValue(item.value).addImage(item.icon).addOptionButton(item.dotsMenu);
+        }
+
+        @Override
+        public void onAttachedToRecyclerView(RecyclerView recyclerView) {
+            super.onAttachedToRecyclerView(recyclerView);
+        }
+
+        @Override
+        public int getItemViewType(int position) {
+            return super.getItemViewType(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return super.getItemId(position);
+        }
+    }
+}
