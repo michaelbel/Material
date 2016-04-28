f44773d4fd1d4a5b360b1fbd66b27e002b544d2e
Michael Bel <michael-bel@outlook.com>
2016-04-28 23:32:15 +0300

    * add static method

---
+++ material/src/main/java/org/app/material/Util.java
@@ -0,0 +1,14 @@
+package org.app.material;
+
+import android.content.Context;
+
+final class Util {
+
+    private Util() {
+    }
+
+    static int dpToPx(Context context, float dp) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return Math.round(dp * scale);
+    }
+}
