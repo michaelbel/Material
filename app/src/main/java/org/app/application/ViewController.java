7d7fa5bf90539ebc20babb42204d6a4dc79382dd
Michael Bel <michael-bel@outlook.com>
2016-05-19 23:45:43 +0300

    Update demo app

--- app/src/main/java/org/app/application/ViewController.java
+++ app/src/main/java/org/app/application/ViewController.java
@@ -18,14 +18,17 @@ package org.app.application;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import org.app.material.AndroidUtilities;
 import org.app.material.MaterialActivity;
 
 public class ViewController extends MaterialActivity {
 
-    Button button;
+    Button button1;
+    Button button2;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -35,5 +38,29 @@ public class ViewController extends MaterialActivity {
         toolbar.setBackButtonIcon(AndroidUtilities.getIcon(this, R.drawable.ic_menu, 0xFFFFFFFF));
         toolbar.addIcon(AndroidUtilities.getIcon(this, R.drawable.ic_dots_menu, 0xFFFFFFFF));
         toolbar.addIcon(AndroidUtilities.getIcon(this, R.drawable.ic_github, 0xFFFFFFFF));
+
+        button1 = new Button(this);
+        button1.setText("Primary");
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        addView(button1);
+
+        button2 = new Button(this);
+        button2.setText("Accent");
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        addView(button2);
+    }
+
+    private void makeToast(String resId) {
+        Toast.makeText(this, "" + resId, Toast.LENGTH_SHORT).show();
     }
 }
