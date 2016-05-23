fe5af19997623d9bd53906e2023b15bd6949980c
Michael Bel <michael-bel@outlook.com>
2016-05-23 18:35:54 +0300

    Finally update in-app browser

--- material/src/main/java/org/app/material/widget/Browser.java
+++ material/src/main/java/org/app/material/widget/Browser.java
@@ -23,87 +23,32 @@ import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Parcelable;
-import android.support.annotation.NonNull;
 
+import org.app.material.AndroidUtilities;
 import org.app.material.R;
 
 public class Browser {
 
-    private int mToolbarColor;
-    private int mShareIcon;
-    private String mShareIconText;
-    private String mUrl;
-    private Context mContext;
-    private boolean isShareIcon = false;
-
-    public Browser(Context context) {
-        this.mContext = context;
-    }
-
-    public Browser setUrl(String address) {
-        this.mUrl = address;
-        return this;
-    }
-
-    public String getUrl() {
-        return mUrl;
-    }
-
-    public Browser setToolbarColor(int color) {
-        this.mToolbarColor = color;
-        return this;
-    }
-
-    public int getToolbarColor() {
-        return mToolbarColor;
-    }
-
-    public Browser setShareIcon(boolean isIcon) {
-        this.isShareIcon = isIcon;
-        return this;
-    }
-
-    public Browser setShareIconHiddenText(@NonNull String text) {
-        this.mShareIconText = text;
-        return this;
-    }
-
-    public String getShareIconHiddenText() {
-        return mShareIconText;
-    }
-
-    public Browser show() {
-        if (mContext == null || mUrl == null) {
-            return this;
-        }
-
+    public static void openUrl(Context context, String url) {
         try {
-            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(mUrl));
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
             intent.putExtra("android.support.customtabs.extra.SESSION", (Parcelable) null);
-            if (mToolbarColor != 0) {
-                intent.putExtra("android.support.customtabs.extra.TOOLBAR_COLOR", getToolbarColor());
-            }
+            intent.putExtra("android.support.customtabs.extra.TOOLBAR_COLOR", AndroidUtilities.getContextColor(context, R.attr.colorPrimary));
             intent.putExtra("android.support.customtabs.extra.TITLE_VISIBILITY", 1);
             Intent actionIntent = new Intent(Intent.ACTION_SEND);
             actionIntent.setType("text/plain");
-            actionIntent.putExtra(Intent.EXTRA_TEXT, Uri.parse(mUrl).toString());
+            actionIntent.putExtra(Intent.EXTRA_TEXT, Uri.parse(url).toString());
             actionIntent.putExtra(Intent.EXTRA_SUBJECT, "");
-            PendingIntent pendingIntent = PendingIntent.getActivity(mContext, 0, actionIntent, PendingIntent.FLAG_ONE_SHOT);
+            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, actionIntent, PendingIntent.FLAG_ONE_SHOT);
             Bundle bundle = new Bundle();
             bundle.putInt("android.support.customtabs.customaction.ID", 0);
-            if (isShareIcon) {
-                bundle.putParcelable("android.support.customtabs.customaction.ICON", BitmapFactory.decodeResource(mContext.getResources(), R.drawable.abc_ic_menu_share_mtrl_alpha));
-            }
-            if (mShareIconText != null) {
-                bundle.putString("android.support.customtabs.customaction.DESCRIPTION", mShareIconText);
-            }
+            bundle.putParcelable("android.support.customtabs.customaction.ICON", BitmapFactory.decodeResource(context.getResources(), R.drawable.abc_ic_menu_share_mtrl_alpha));
+            bundle.putString("android.support.customtabs.customaction.DESCRIPTION", "Share link");
             bundle.putParcelable("android.support.customtabs.customaction.PENDING_INTENT", pendingIntent);
             intent.putExtra("android.support.customtabs.extra.ACTION_BUTTON_BUNDLE", bundle);
             intent.putExtra("android.support.customtabs.extra.TINT_ACTION_BUTTON", false);
-            intent.putExtra(android.provider.Browser.EXTRA_APPLICATION_ID, mContext.getPackageName());
-            mContext.startActivity(intent);
+            intent.putExtra(android.provider.Browser.EXTRA_APPLICATION_ID, context.getPackageName());
+            context.startActivity(intent);
         } catch (Exception ignored) {}
-
-        return this;
     }
 }
