9b81380ed8228858aec578fc0222e102e8497826
Michael Bel <michael-bel@outlook.com>
2016-05-23 18:50:47 +0300

    Update activity

--- app/src/main/java/org/app/application/ViewController.java
+++ app/src/main/java/org/app/application/ViewController.java
@@ -18,49 +18,47 @@ package org.app.application;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.v4.app.FragmentActivity;
+import android.widget.LinearLayout;
 
 import org.app.material.AndroidUtilities;
-import org.app.material.MaterialActivity;
+import org.app.material.widget.LayoutHelper;
+import org.app.material.widget.Toolbar;
 
-public class ViewController extends MaterialActivity {
+public class ViewController extends FragmentActivity {
 
-    Button button1;
-    Button button2;
+    public Toolbar toolbar;
+    public LinearLayout viewLayout;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        toolbar.setTitle("View Controller");
-        toolbar.setBackButtonIcon(AndroidUtilities.getIcon(this, R.drawable.ic_menu, 0xFFFFFFFF));
-        toolbar.addIcon(AndroidUtilities.getIcon(this, R.drawable.ic_dots_menu, 0xFFFFFFFF));
-        toolbar.addIcon(AndroidUtilities.getIcon(this, R.drawable.ic_github, 0xFFFFFFFF));
+        CoordinatorLayout containerLayout = new CoordinatorLayout(this);
+        containerLayout.setLayoutParams(LayoutHelper.makeCoordinator(this, LayoutHelper.MATCH_PARENT, LayoutHelper.MATCH_PARENT));
 
-        button1 = new Button(this);
-        button1.setText("Primary");
-        button1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        toolbar = new Toolbar(this);
+        toolbar.setTitle(R.string.MaterialDemo);
+        toolbar.setNavIcon(R.drawable.ic_menu);
+        //toolbar.setNavIconClickListener(new View.OnClickListener() {
+        //    @Override
+        //    public void onClick(View v) {
+        //        Toast.makeText(ViewController.this, "Menus", Toast.LENGTH_SHORT).show();
+        //    }
+        //});
 
-            }
-        });
-        addView(button1);
+        toolbar.addIcon(R.drawable.ic_dots_menu);
+        toolbar.addIcon(R.drawable.ic_github);
 
-        button2 = new Button(this);
-        button2.setText("Accent");
-        button2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        containerLayout.addView(toolbar);
 
-            }
-        });
-        addView(button2);
-    }
+        viewLayout = new LinearLayout(this);
+        viewLayout.setOrientation(LinearLayout.VERTICAL);
+        viewLayout.setPadding(0, AndroidUtilities.dp(this, 56), 0, 0);
+        viewLayout.setLayoutParams(LayoutHelper.makeLinear(this, LayoutHelper.MATCH_PARENT, LayoutHelper.MATCH_PARENT));
+        containerLayout.addView(viewLayout);
 
-    private void makeToast(String resId) {
-        Toast.makeText(this, "" + resId, Toast.LENGTH_SHORT).show();
+        setContentView(containerLayout);
     }
 }
