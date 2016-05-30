f4a373c65736113cbba2250e12553f6b5a838c0e
Michael Bel <michael-bel@outlook.com>
2016-05-30 21:04:10 +0300

    Add getter for the status bar height

--- material/src/main/java/org/app/material/AndroidUtilities.java
+++ material/src/main/java/org/app/material/AndroidUtilities.java
@@ -96,4 +96,15 @@ public class AndroidUtilities {
 
         return backgroundResource;
     }
+
+    public static int getStatusBarHeight(Context context) {
+        int result = 0;
+        int resourceId = context.getResources().getIdentifier("status_bar_height", "dimen", "android");
+
+        if (resourceId > 0) {
+            result = context.getResources().getDimensionPixelSize(resourceId);
+        }
+
+        return result ;
+    }
 }
