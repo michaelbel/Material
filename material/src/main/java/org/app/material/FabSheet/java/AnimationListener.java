c4bb3bdadb8ebc128ad0fa1b544b8573c53da0d5
Michael Bel <michael-bel@outlook.com>
2016-06-11 12:07:19 +0300

    Delete files

--- material/src/main/java/org/app/material/FabSheet/java/AnimationListener.java
+++
@@ -1,8 +0,0 @@
-package org.app.material.FabSheet.java;
-
-public abstract class AnimationListener extends ViewAnimationUtils.SimpleAnimationListener {
-
-	public void onStart() {}
-
-	public void onEnd() {}
-}
