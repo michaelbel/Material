297d96f4e1bcce5c1cb1748447abe598295c266f
Michael Bel <michael-bel@outlook.com>
2016-06-11 16:24:58 +0300

    Update loader

---
+++ app/src/main/java/org/app/application/ApplicationLoader.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2015 Michael Bel
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.app.application;
+
+import android.app.Application;
+import android.content.Context;
+import android.os.Handler;
+
+public class ApplicationLoader extends Application {
+
+    public static volatile Handler applicationHandler;
+    public static volatile Context applicationContext;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        applicationContext = getApplicationContext();
+        applicationHandler = new Handler(getApplicationContext().getMainLooper());
+    }
+}
