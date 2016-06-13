6edb4c0f50c0cc51c379c4b3067defa9380b35c9
Michael Bel <michael-bel@outlook.com>
2016-06-13 22:44:27 +0300

    add methods to display and hide the keyboard

--- material/src/main/java/org/app/material/AndroidUtilities.java
+++ material/src/main/java/org/app/material/AndroidUtilities.java
@@ -28,7 +28,9 @@ import android.os.Handler;
 import android.util.DisplayMetrics;
 import android.util.TypedValue;
 import android.view.Display;
+import android.view.View;
 import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
 
 import java.util.Hashtable;
 
@@ -172,5 +174,32 @@ public class AndroidUtilities {
         }
 
         return result ;
+    }
+
+    public static void showKeyboard(View view) {
+        if (view == null) {
+            return;
+        }
+        InputMethodManager inputManager = (InputMethodManager)view.getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+        inputManager.showSoftInput(view, InputMethodManager.SHOW_IMPLICIT);
+    }
+
+    public static boolean isKeyboardShowed(View view) {
+        if (view == null) {
+            return false;
+        }
+        InputMethodManager inputManager = (InputMethodManager) view.getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+        return inputManager.isActive(view);
+    }
+
+    public static void hideKeyboard(View view) {
+        if (view == null) {
+            return;
+        }
+        InputMethodManager imm = (InputMethodManager) view.getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+        if (!imm.isActive()) {
+            return;
+        }
+        imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
     }
 }
