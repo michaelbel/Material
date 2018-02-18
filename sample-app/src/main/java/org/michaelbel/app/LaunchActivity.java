177f2b575840be6b0fe223b8fb168101bc093f2b
Michael Bel <michael-bel@outlook.com>
2018-02-18 13:52:50 +0300

    Update activity

--- sample-app/src/main/java/org/michaelbel/app/LaunchActivity.java
+++ sample-app/src/main/java/org/michaelbel/app/LaunchActivity.java
@@ -1,6 +1,5 @@
 package org.michaelbel.app;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.FragmentActivity;
@@ -25,19 +24,19 @@ public class LaunchActivity extends FragmentActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-        ActionBar actionBar = (ActionBar) findViewById(R.id.action_bar);
+        ActionBar actionBar = findViewById(R.id.action_bar);
         actionBar.setNavigationIcon(R.drawable.ic_menu);
         actionBar.setTitle(R.string.MaterialDemo);
         actionBar.setActionBarMenuOnItemClick(new ActionBar.ActionBarMenuOnItemClick() {
             @Override
             public void onItemClick(int id) {
                 if (id == ActionBar.MENU_ICON) {
-                    startActivity(new Intent(LaunchActivity.this, ActionmodActivity.class));
+                    //startActivity(new Intent(LaunchActivity.this, ActionmodActivity.class));
                 } else if (id == github) {
                     //Browser.openUrl(LaunchActivity.this, R.string.GithubURL);
-                    startActivity(new Intent(LaunchActivity.this, MainActivity.class));
+                    //startActivity(new Intent(LaunchActivity.this, MainActivity.class));
                 } else if (id == settings) {
-                    startActivity(new Intent(LaunchActivity.this, SearchableActivity.class));
+                    //startActivity(new Intent(LaunchActivity.this, SearchableActivity.class));
                 }
             }
         });
@@ -50,7 +49,7 @@ public class LaunchActivity extends FragmentActivity {
 
         item.addSubItem(settings, R.string.Settings);
 
-        ViewPager viewPager = (ViewPager) findViewById(R.id.view_pager);
+        ViewPager viewPager = findViewById(R.id.view_pager);
 
         FragmentsPagerAdapter adapter = new FragmentsPagerAdapter(this, getSupportFragmentManager());
         adapter.addFragment(new DialogsFragment(), R.string.Dialogs);
@@ -58,7 +57,7 @@ public class LaunchActivity extends FragmentActivity {
         adapter.addFragment(new FabFragment(), R.string.Fabs);
         viewPager.setAdapter(adapter);
 
-        TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
+        TabLayout tabLayout = findViewById(R.id.tab_layout);
         tabLayout.setTabMode(TabLayout.MODE_SCROLLABLE);
         tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
         tabLayout.setBackgroundColor(Utils.getAttrColor(this, R.attr.colorPrimary));
@@ -75,10 +74,5 @@ public class LaunchActivity extends FragmentActivity {
             @Override
             public void onTabReselected(TabLayout.Tab tab) {}
         });
-    }
-
-    @Override
-    public void onBackPressed() {
-        super.onBackPressed();
     }
 }
