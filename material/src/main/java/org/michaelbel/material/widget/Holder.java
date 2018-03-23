de59549e435cb11bb2f3731e9949fbf67049f522
Michael Bel <michael-bel@outlook.com>
2018-03-23 22:31:11 +0300

    Update material library

---
+++ material/src/main/java/org/michaelbel/material/widget/Holder.java
@@ -0,0 +1,19 @@
+package org.michaelbel.material.widget;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+/**
+ * Date: 23 MAR 2018
+ * Time: 22:30 MSK
+ *
+ * @author Michael Bel
+ */
+
+@SuppressWarnings("all")
+public class Holder extends RecyclerView.ViewHolder {
+
+    public Holder(View view) {
+        super(view);
+    }
+}
